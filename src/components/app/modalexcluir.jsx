4ae802cf7ab699f3168144bf37b3206ea7e864c1
camilly <vicpere5hv@gmail.com>
2025-03-28 15:31:33 -0300

    ,

--- src/components/app/modalexcluir.jsx
+++
@@ -1,9 +0,0 @@
-function excluir () {
-    return(
-        <div>
-            <label>deseja excluir o usuario?</label>
-            <button>aceitar</button>
-            <button>concelar</button>
-        </div>
-    )
-}
